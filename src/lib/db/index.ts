21532502d14177ed7e0d4edefa6d2af46caf617c
nayyirahsan <nayyirahsan@gmail.com>
2026-05-12 02:04:40 -0500

    fix pooler connection

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,25 +1,12 @@
-import postgres from "postgres";
-import { drizzle } from "drizzle-orm/postgres-js";
-import * as schema from "./schema";
+import postgres from 'postgres'
+import { drizzle } from 'drizzle-orm/postgres-js'
+import * as schema from './schema'
 
-type DrizzleDb = ReturnType<typeof drizzle<typeof schema>>;
+const client = postgres(process.env.DATABASE_URL!, {
+  max: 1,
+  idle_timeout: 20,
+  connect_timeout: 10,
+  prepare: false,
+})
 
-let _db: DrizzleDb | undefined;
-
-function getDb(): DrizzleDb {
-  if (!_db) {
-    const client = postgres(process.env.DATABASE_URL!, {
-      max: 1,
-      idle_timeout: 20,
-      connect_timeout: 10,
-    });
-    _db = drizzle(client, { schema });
-  }
-  return _db;
-}
-
-export const db: DrizzleDb = new Proxy({} as DrizzleDb, {
-  get(_, prop) {
-    return (getDb() as any)[prop];
-  },
-});
+export const db = drizzle(client, { schema })
